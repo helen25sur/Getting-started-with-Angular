c7f2664cace5756e7abec44b747feabe5302e9a8
Elena Surilova <elen21surilova@gmail.com>
2023-03-27 00:26:54 +0300

    feat: add form to cart page
    add form for user data: name & address to cart component

--- start-angular/src/app/app.module.ts
+++ start-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
 import { AppProductListComponent } from './app-product-list/app-product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 // Angular Material
 import {MatCommonModule} from '@angular/material/core';
@@ -16,8 +19,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -34,12 +37,15 @@ import { ShippingComponent } from './shipping/shipping.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     // Angular Material
     MatCommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatListModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
